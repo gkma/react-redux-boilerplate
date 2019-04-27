58c30ff6b2fb2ec8e36e739bf0787e50e4f6f4be
Garron Ma <garronma@gmail.com>
2019-04-27 15:54:00 -0700

    [4-27-19] Added comments for documentation

--- src/client/constants/actionTypes.js
+++ src/client/constants/actionTypes.js
@@ -1,3 +1,11 @@
+/**
+ * "actionTypes.js"
+ *
+ * List of variable constants containing their string equivalent
+ * Stored within one object, actionTypes, which can access all
+ */
+
+// !-- ACTION TYPES --! //
 const actionTypes = {
   CHANGE_TEXT: "CHANGE_TEXT",
   RESET_TEXT: "RESET_TEXT"
