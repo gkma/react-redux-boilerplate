ee29a174ef7209cda94968e6280791eb282b4f64
Garron Ma <garronma@gmail.com>
2019-04-27 15:26:56 -0700

    [4-27-19] Add provider wrapper to App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import App from "./client/containers/App.jsx";
+import store from "./store";
 
 const rootDivElement = document.querySelector("#root");
 
-ReactDOM.render(<h1>React | Redux Boilerplate</h1>, rootDivElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootDivElement
+);
