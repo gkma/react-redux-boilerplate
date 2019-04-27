e04402804fbdf92d0f4013021d0cb50fd61f3975
Garron Ma <garronma@gmail.com>
2019-04-27 12:21:27 -0700

    [4-27-19] Update package and removed del files

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const rootDivElement = document.querySelector("#root");
-
-ReactDOM.render(<h1>React | Redux Boilerplate</h1>, rootDivElement);
