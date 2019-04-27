74daac1d5b6dcf79c86b0dba4585ac2529d5e7b6
Garron Ma <garronma@gmail.com>
2019-04-27 15:51:13 -0700

    [4-27-19] Added comments for documentation

--- src/client/actions/actionCreator.js
+++ src/client/actions/actionCreator.js
@@ -1,5 +1,15 @@
+/**
+ * "actionCreator.js"
+ *
+ * List of functions that return an "actionable" object or function (thunk)
+ * Input: type, payload (optional)
+ * Output: action object or function
+ */
+
+// !-- IMPORT TYPES --! //
 import types from "../constants/actionTypes";
 
+// !-- DEFAULT ACTION CREATORS --! //
 export const changeText = payload => ({
   type: types.CHANGE_TEXT,
   payload
