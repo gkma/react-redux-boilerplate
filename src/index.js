1136adc073e065ae009dad7a541f7fc31951ab91
Garron Ma <garronma@gmail.com>
2019-04-27 12:20:13 -0700

    [4-27-19] Add index.js to src

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootDivElement = document.querySelector("#root");
+
+ReactDOM.render(<h1>React | Redux Boilerplate</h1>, rootDivElement);
